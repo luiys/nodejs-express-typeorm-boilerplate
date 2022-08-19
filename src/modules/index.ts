d0e64f31bfeafa7bd93b993b0e5009aa9cddc4af
Luis Gustavo <Luisgustavofeitoza@gmail.com>
2022-08-19 00:09:08 -0300

    configurações para o Prisma

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,9 +1,7 @@
-import { UserController } from './Acesso/UserController'
 import { LogController } from './Sistema/LogController'
 import { SistemaController } from './Sistema/SistemaController'
 
 export const controllers = [
-    UserController,
     SistemaController,
     LogController
 ]
